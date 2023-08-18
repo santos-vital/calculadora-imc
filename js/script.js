877f905f39ca4d2fd1923003ad3360baf439c1b5
santos-vital <vital.leandro16@gmail.com>
2023-08-17 22:17:11 -0300

    Modularizando a aplicação

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
+import { Modal } from './modal.js';
+
 // variaveis
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
-
-//Estrutura de dados object literals
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  btnClose: document.querySelector('.modal button.close'), 
-
-  open() {
-    this.wrapper.classList.add('open');
-  },
-  close() {
-    this.wrapper.classList.remove('open');
-  }
-}
 
 // 3 maneiras de criar e atribuir função a um evento
 //1
@@ -35,10 +23,6 @@ form.onsubmit = function(event) {
 // form.onsubmit = () => {};
 // 3
 // form.onsubmit = handleSubmit;
-
-Modal.btnClose.onclick = () => {
-  Modal.close();
-}
 
 function IMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
